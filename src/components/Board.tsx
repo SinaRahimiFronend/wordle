78399e6b42748f67a01f628cb0731b7f2662487a
rahimi <rahimi@citynet.ir>
2024-05-26 12:22:28 +0330

    renamed isLost variable to isDefeat

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,13 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
-import ResultDialog from './ResultDialog';
-import { TilesRow } from '../types/types';
-import Tile from './Tile';
-import Keyboard from './Keyboard';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import ResultDialog from "./ResultDialog";
+import { TilesRow } from "../types/types";
+import Tile from "./Tile";
+import Keyboard from "./Keyboard";
 
-const hiddenWord = 'PLINE';
+const hiddenWord = "PLINE";
 
 export default function Board() {
-  const [words, setWords] = useState<string[]>(['', '', '', '', '', '']);
+  const [words, setWords] = useState<string[]>(["", "", "", "", "", ""]);
   const [activeTry, setActiveTry] = useState(0);
 
   const rows = useMemo<TilesRow[]>(() => {
@@ -17,15 +17,15 @@ export default function Board() {
       for (let j = 0; j < 5; j++) {
         newRow.tiles.push({
           id: String(i) + String(j),
-          value: words[i][j] || '',
+          value: words[i][j] || "",
           status:
             activeTry > i
               ? hiddenWord[j] === words[i][j]
-                ? 'correct'
+                ? "correct"
                 : hiddenWord.includes(words[i][j])
-                ? 'misplaced'
-                : 'wrong'
-              : 'empty',
+                ? "misplaced"
+                : "wrong"
+              : "empty",
         });
       }
       newRows.push(newRow);
@@ -36,24 +36,24 @@ export default function Board() {
   const handleKeyUp = useCallback(
     (key: string) => {
       if (activeTry > words.length) return;
-      if (key >= 'a' && key <= 'z') {
-        setWords(prevWords => {
+      if (key >= "a" && key <= "z") {
+        setWords((prevWords) => {
           const newWords = [...prevWords];
           if (newWords[activeTry].length < 5) {
             newWords[activeTry] += key.toLocaleUpperCase();
           }
           return newWords;
         });
-      } else if (key === 'Backspace') {
-        setWords(prevWords => {
+      } else if (key === "Backspace") {
+        setWords((prevWords) => {
           const newWords = [...prevWords];
           newWords[activeTry] = newWords[activeTry].slice(0, -1);
 
           return newWords;
         });
-      } else if (key === 'Enter') {
-        if (words[activeTry].length === 5) setActiveTry(prev => prev + 1);
-        else console.log('wrong');
+      } else if (key === "Enter") {
+        if (words[activeTry].length === 5) setActiveTry((prev) => prev + 1);
+        else console.log("wrong");
       }
     },
     [activeTry, words]
@@ -64,27 +64,27 @@ export default function Board() {
       handleKeyUp(e.key);
     }
 
-    document.addEventListener('keyup', handleKeyPress);
+    document.addEventListener("keyup", handleKeyPress);
 
     return () => {
-      document.removeEventListener('keyup', handleKeyPress);
+      document.removeEventListener("keyup", handleKeyPress);
     };
   }, [handleKeyUp]);
 
   function handleGameReset() {
-    setWords(['', '', '', '', '', '']);
+    setWords(["", "", "", "", "", ""]);
     setActiveTry(0);
   }
 
-  const matchedWordIndex = words.findIndex(word => word === hiddenWord);
+  const matchedWordIndex = words.findIndex((word) => word === hiddenWord);
 
-  const isLost = matchedWordIndex === -1 && activeTry === words.length;
+  const isDefeat = matchedWordIndex === -1 && activeTry === words.length;
   const isVictory = matchedWordIndex !== -1 && activeTry > matchedWordIndex;
 
   return (
     <div>
       <div className="text-white mx-auto w-max max-w-l p-1 mt-12 flex flex-col gap-2">
-        {rows.map(row => (
+        {rows.map((row) => (
           <Row key={row.id} row={row} />
         ))}
       </div>
@@ -92,14 +92,14 @@ export default function Board() {
         handleKeyPress={handleKeyUp}
         checkedLetters={rows
           .slice(0, activeTry)
-          .map(row => row.tiles)
+          .map((row) => row.tiles)
           .flat()
-          .filter(tile => !!tile.value)}
+          .filter((tile) => !!tile.value)}
       />
       {isVictory && (
         <ResultDialog text="You Have won🎉" handleReset={handleGameReset} />
       )}
-      {isLost && (
+      {isDefeat && (
         <ResultDialog text="You Have Lost!!😥" handleReset={handleGameReset} />
       )}
     </div>
@@ -109,7 +109,7 @@ export default function Board() {
 function Row({ row }: { row: TilesRow }) {
   return (
     <div className="flex gap-2">
-      {row.tiles.map(tile => (
+      {row.tiles.map((tile) => (
         <Tile key={tile.id} tile={tile} />
       ))}
     </div>
