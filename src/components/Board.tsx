7c699f775e617ab5447418d6140db721d9b0b954
seeno <sina.leagueoflegends@gmail.com>
2024-05-26 05:34:55 +0330

    added win and lose dialog and game reset functionality

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
+import ResultDialog from './ResultDialog';
 
 interface Row {
   id: number;
@@ -13,7 +14,7 @@ interface Tile {
 }
 
 export default function Board() {
-  const [letters, setLetters] = useState<string[]>(['', '', '', '', '']);
+  const [words, setWords] = useState<string[]>(['', '', '', '', '']);
   const [activeTry, setActiveTry] = useState(0);
 
   const [hiddenWord, setHiddenWord] = useState('PLINE');
@@ -25,11 +26,11 @@ export default function Board() {
       for (let j = 0; j < 5; j++) {
         newRow.tiles.push({
           id: String(i) + String(j),
-          value: letters[i][j] || '',
+          value: words[i][j] || '',
           status:
-            hiddenWord[j] === letters[i][j]
+            hiddenWord[j] === words[i][j]
               ? 'correct'
-              : hiddenWord.includes(letters[i][j])
+              : hiddenWord.includes(words[i][j])
               ? 'exists'
               : 'wrong',
         });
@@ -37,31 +38,31 @@ export default function Board() {
       newRows.push(newRow);
     }
     return newRows;
-  }, [letters, activeTry, hiddenWord]);
+  }, [words, activeTry, hiddenWord]);
 
   const handleKeyUp = useCallback(
     (e: KeyboardEvent) => {
       if (e.key >= 'a' && e.key <= 'z') {
-        setLetters(prevLetters => {
-          const newLetters = [...prevLetters];
-          if (newLetters[activeTry].length < 5) {
-            newLetters[activeTry] += e.key.toLocaleUpperCase();
+        setWords(prevWords => {
+          const newWords = [...prevWords];
+          if (newWords[activeTry].length < 5) {
+            newWords[activeTry] += e.key.toLocaleUpperCase();
           }
-          return newLetters;
+          return newWords;
         });
       } else if (e.key === 'Backspace') {
-        setLetters(prevLetters => {
-          const newLetters = [...prevLetters];
-          newLetters[activeTry] = newLetters[activeTry].slice(0, -1);
+        setWords(prevWords => {
+          const newWords = [...prevWords];
+          newWords[activeTry] = newWords[activeTry].slice(0, -1);
 
-          return newLetters;
+          return newWords;
         });
       } else if (e.key === 'Enter') {
-        if (letters[activeTry].length === 5) setActiveTry(prev => prev + 1);
+        if (words[activeTry].length === 5) setActiveTry(prev => prev + 1);
         else console.log('wrong');
       }
     },
-    [activeTry, letters]
+    [activeTry, words]
   );
 
   useEffect(() => {
@@ -72,11 +73,27 @@ export default function Board() {
     };
   }, [handleKeyUp]);
 
+  function handleGameReset() {
+    setWords(['', '', '', '', '']);
+    setActiveTry(0);
+  }
+
+  const matchedWordIndex = words.findIndex(word => word === hiddenWord);
+
+  const isLost = matchedWordIndex === -1 && activeTry === 5;
+  const isVictory = matchedWordIndex !== -1 && activeTry > matchedWordIndex;
+
   return (
     <div className="text-white mx-auto w-max max-w-l p-1 mt-12 flex flex-col gap-2">
       {rows.map((row, i) => (
         <Row key={row.id} row={row} active={activeTry > i} />
       ))}
+      {isVictory && (
+        <ResultDialog text="You Have won🎉" handleReset={handleGameReset} />
+      )}
+      {isLost && (
+        <ResultDialog text="You Have Lost!!😥" handleReset={handleGameReset} />
+      )}
     </div>
   );
 }
@@ -111,7 +128,7 @@ function Tile({ tile, active }: { tile: Tile; active: boolean }) {
   }
   return (
     <div
-      className="border-2 h-14 w-14 flex items-center justify-center text-3xl"
+      className="border-2 border-zinc-700 h-14 w-14 flex items-center justify-center text-3xl"
       style={{
         backgroundColor: bgColor,
       }}
